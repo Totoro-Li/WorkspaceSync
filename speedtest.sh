cf99a02181895f3bfcc721ddb437dee1a9394dc5
Totoro-Li <90897313+Totoro-Li@users.noreply.github.com>
2023-09-14 17:26:24 +0800

    aa

--- speedtest.sh
+++ speedtest.sh
@@ -3,53 +3,56 @@
 declare -A write_speeds
 declare -A read_speeds
 
+# List of drives to be tested
+drives=("HDD1" "HDD2" "HDD3" "HDD_RAID" "SSD1")
+
 # Test write speeds
-for i in 1 2 3 4
+for drive in "${drives[@]}"
 do
-    echo "Testing write speed for HDD$i..."
-    result=$(dd if=/dev/zero of=/mnt/HDD$i/testfile bs=1G count=1 oflag=direct 2>&1)
-    rm -f /mnt/HDD$i/testfile
+    echo "Testing write speed for $drive..."
+    result=$(dd if=/dev/zero of=/mnt/$drive/testfile bs=1G count=1 oflag=direct 2>&1)
+    rm -f /mnt/$drive/testfile
     speed=$(echo $result | grep -oP '\d+(\.\d+)? MB/s' | grep -oP '\d+(\.\d+)?')
-    write_speeds["HDD$i"]=$speed
-    echo "HDD$i Write Speed: $speed MB/s"
+    write_speeds["$drive"]=$speed
+    echo "$drive Write Speed: $speed MB/s"
 done
 
 # Test read speeds
-for i in 1 2 3 4
+for drive in "${drives[@]}"
 do
-    dd if=/dev/zero of=/mnt/HDD$i/testfile bs=1G count=1 oflag=direct > /dev/null 2>&1
-    echo "Testing read speed for HDD$i..."
-    result=$(dd if=/mnt/HDD$i/testfile of=/dev/null bs=1G count=1 iflag=direct 2>&1)
-    rm -f /mnt/HDD$i/testfile
+    dd if=/dev/zero of=/mnt/$drive/testfile bs=1G count=1 oflag=direct > /dev/null 2>&1
+    echo "Testing read speed for $drive..."
+    result=$(dd if=/mnt/$drive/testfile of=/dev/null bs=1G count=1 iflag=direct 2>&1)
+    rm -f /mnt/$drive/testfile
     speed=$(echo $result | grep -oP '\d+(\.\d+)? MB/s' | grep -oP '\d+(\.\d+)?')
-    read_speeds["HDD$i"]=$speed
-    echo "HDD$i Read Speed: $speed MB/s"
+    read_speeds["$drive"]=$speed
+    echo "$drive Read Speed: $speed MB/s"
 done
 
 # Sort and output the results
 echo "Write Speeds from High to Low:"
-for hdd in $(echo "${write_speeds[@]}" | tr ' ' '\n' | sort -rn)
+for speed in $(echo "${write_speeds[@]}" | tr ' ' '\n' | sort -rn)
 do
-    for hddname in "${!write_speeds[@]}"
+    for drive in "${!write_speeds[@]}"
     do
-        if [[ "${write_speeds[$hddname]}" == "$hdd" ]]
+        if [[ "${write_speeds[$drive]}" == "$speed" ]]
         then
-            echo "$hddname: $hdd MB/s"
-            unset write_speeds[$hddname]
+            echo "$drive: $speed MB/s"
+            unset write_speeds[$drive]
             break
         fi
     done
 done
 
 echo "Read Speeds from High to Low:"
-for hdd in $(echo "${read_speeds[@]}" | tr ' ' '\n' | sort -rn)
+for speed in $(echo "${read_speeds[@]}" | tr ' ' '\n' | sort -rn)
 do
-    for hddname in "${!read_speeds[@]}"
+    for drive in "${!read_speeds[@]}"
     do
-        if [[ "${read_speeds[$hddname]}" == "$hdd" ]]
+        if [[ "${read_speeds[$drive]}" == "$speed" ]]
         then
-            echo "$hddname: $hdd MB/s"
-            unset read_speeds[$hddname]
+            echo "$drive: $speed MB/s"
+            unset read_speeds[$drive]
             break
         fi
     done
